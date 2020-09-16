3a6ff8ec0db24cff41992f899f368095ed197c99
Charlie Liu <charliez.liu@gmail.com>
2020-09-16 23:24:04 +1200

    Added feature to rebind keys

---
+++ AUT Fighters/Assets/Charlie/Scripts/KeyBindScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class KeyBindScript : MonoBehaviour
+{
+    private Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
+
+    public TMP_Text up, down, left, right;
+
+    public GameObject currentKey;
+
+    private Color32 normal = new Color32(255, 255, 255, 255);
+    private Color32 selected = new Color32(239, 116, 36, 255);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        keys.Add("Up", KeyCode.W);
+        keys.Add("Down", KeyCode.S);
+        keys.Add("Left", KeyCode.A);
+        keys.Add("Right", KeyCode.D);
+
+        up.text = keys["Up"].ToString();
+        down.text = keys["Down"].ToString();
+        left.text = keys["Left"].ToString();
+        right.text = keys["Right"].ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    private void OnGUI()
+    {
+        if (currentKey != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                keys[currentKey.name] = e.keyCode;  // Rebind move with current button pressed
+                currentKey.transform.GetChild(0).GetComponent<TMP_Text>().text = e.keyCode.ToString();  // Change text on button
+                currentKey.GetComponent<Image>().color = normal;
+                currentKey = null;
+            }
+        }
+    }
+
+
+    public void changeKey(GameObject clicked)
+    {
+        // Recolour old key when selecting new key
+        if (currentKey != null)
+        {
+            currentKey.GetComponent<Image>().color = normal;
+        }
+
+        currentKey = clicked;
+        currentKey.GetComponent<Image>().color = selected;
+    }
+}

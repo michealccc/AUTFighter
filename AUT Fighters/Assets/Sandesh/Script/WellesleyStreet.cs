ab72af25223c71491d63e12018b833b0b12b6864
SandeshPathak <sandesh.pathak07@gmail.com>
2020-09-25 10:37:01 +1200

    Scripts attached to the First 2 Stages
    Scripts attached to Wellesley Street and StPaulStreet with their respective scenes that loads when the button is pressed

---
+++ AUT Fighters/Assets/Sandesh/Script/WellesleyStreet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WellesleyStreet : MonoBehaviour
+{
+    public void SceneChanger()
+    {
+        SceneManager.LoadScene("WellesleyStreetStage");
+    }
+
+
+}

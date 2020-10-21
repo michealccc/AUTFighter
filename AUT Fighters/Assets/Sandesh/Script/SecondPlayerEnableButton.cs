13dd1b5363764e1532e2c12f6746ecc7c9a1cffd
SandeshPathak <sandesh.pathak07@gmail.com>
2020-10-21 19:50:54 +1300

    Made changes to CharacterSelect Stage
    CharacterSelect stage is used for Verzus mode for double player, made all the changes required for that, added scripts, UI elements.

---
+++ AUT Fighters/Assets/Sandesh/Script/SecondPlayerEnableButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using static UnityEngine.UI.Button;
+
+
+public class SecondPlayerEnableButton : MonoBehaviour
+{
+    //This script makes the confirm button interactable and sets the Image to be active
+    //when the characters are selected
+    public Button SecondPlayer1;
+    public Button SecondPlayer2;
+    public Button SecondPlayer3;
+    public Button SecondPlayer4;
+    public Button secondplayerImage;
+    public Button confirmButton;
+
+
+
+    public void ButtonEnabler()
+    {
+        secondplayerImage.gameObject.SetActive(true);
+        confirmButton.interactable = true;
+    }
+
+
+}

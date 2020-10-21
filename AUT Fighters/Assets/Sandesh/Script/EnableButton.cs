73925620c6639a0f0cd4a2028aa75c4e9beaafbb
SandeshPathak <sandesh.pathak07@gmail.com>
2020-10-21 13:07:40 +1300

    Scripts, Character Images added
    Added 3 scripts, to disable the confirm button, to enable it when the character is selected and image changer that changes the image when different characters are selected

---
+++ AUT Fighters/Assets/Sandesh/Script/EnableButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using static UnityEngine.UI.Button;
+
+
+public class EnableButton : MonoBehaviour
+{
+    //This script makes the confirm button interactable and sets the Image to be active
+    //when the characters are selected
+    public Button Player1;
+    public Button Player2;
+    public Button Player3;
+    public Button Player4;
+    public Button firstplayerImage;
+    public Button confirmButton;
+
+
+    public void ButtonEnabler()
+    {
+        firstplayerImage.gameObject.SetActive(true);
+        confirmButton.interactable = true;
+
+    }
+
+
+}

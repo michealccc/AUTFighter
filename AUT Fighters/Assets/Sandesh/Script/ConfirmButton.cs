2cc1b5286bce008f53eefea488f874da327774cc
SandeshPathak <sandesh.pathak07@gmail.com>
2020-10-24 11:44:39 +1300

    Made the confirm Button uninteractable at the start
    Made the ConfirmStageButton uninteractable at the start and only interactable when one of the stages are clicked

---
+++ AUT Fighters/Assets/Sandesh/Script/ConfirmButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmButton : MonoBehaviour
+{
+    public Button confirmbutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmbutton.interactable = false;
+    }
+
+    // Update is called once per frame
+    public void enableButton()
+    {
+        confirmbutton.interactable = true;
+
+    }
+}
+
